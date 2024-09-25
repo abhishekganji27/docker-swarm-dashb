de954b069a7d8f76d97f3ff0e3a9efa43fa0c06a
Abhishek Ganji <abhishekganji27@gmail.com>
2024-09-25 17:24:58 +0530

    Modified many files

--- Dockerfile
+++ Dockerfile
@@ -18,4 +18,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["flask", "run", "--debug"]
+CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -1,37 +1,32 @@
 from flask import Flask, render_template, jsonify
 import docker
+
 # client = None
 app = Flask(__name__)
 
+def get_client():
+    return docker.from_env()
+
 @app.route('/')
 def hello_world():
-    client = docker.from_env()
+    client = get_client()
     p = client.ping()
     imgs = get_images(client)
     return render_template('index.html', ping = p, images = imgs)
     # return jsonify({"ping": p, })
 
+# Swarm Nodes API
+@app.route("/nodes")
+def list_swarm_nodes():
+    client = get_client()
+    nlist = client.nodes.list()
+    return render_template('list.html', res_list = nlist)
+
+
+# Images 
 def get_images(client):
-    try:
+    try: 
         res = client.images.list()
         return res
     except docker.errors.APIError as a:
         print("Error: ",a)
-
-"""
-Make an engine join a docker swarm that has already been created.
-Params: 
-1. remote_addrs(list[str]): IP address of one or more managers. 
-2. join_token(str): Secret token to join the swarm.
-3. advertise_addr(str): IP of node running this web application.
-
-@app.route("/join-swarm")
-def join_swarm(ra, jt, aa):
-    try:
-        response = client.swarm.join(remote_addrs=[str(ra)], join_token=jt, advertise_addr=aa)
-        return response
-    except docker.errors.APIError as e:
-        print("An errored occured", e)
-"""
-if __name__ == "__main__":
-    app.run(debug=True)
