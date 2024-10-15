7d7cc8ccca23c6a3a83e61a2e21340d8a37a3098
Abhishek Ganji <abhishekganji27@gmail.com>
2024-10-15 12:10:30 +0530

    json responses

--- app.py
+++ app.py
@@ -2,51 +2,68 @@ from flask import Flask, render_template, jsonify, flash
 import docker
 import docker.errors as de
 
-# client = None
 app = Flask(__name__)
 
 def get_client():
     return docker.from_env()
 
+# Home
 @app.route('/')
 def home():
     client = get_client()
     p = client.ping()
-    return render_template('index.html', ping = p)
+    return jsonify( {"ping": p} )
+    # return render_template('index.html', ping = p)
 
-# Swarm Services
+# Swarm Services - List
 @app.route("/services")
-def list_swarm_services():
+def swarm_services_list():
     try:
         client = get_client()
         slist = client.services.list()
-        return render_template("services/list.html", res_list = slist)
+        return jsonify( { "services list" : slist } )
+        # return render_template("services/list.html", res_list = slist)
     except de.APIError as e:
         flash("Error!")
-        return render_template("404.html", error=e)
+        return jsonify( { "error" : e } )
+        # return render_template("404.html", error=e)
 
+
+# Swarm Services - Inspect
 @app.route("/services/inspect/<id>")
-def inspect_swarm_service(id):
+def swarm_service_inspect(id):
     try:
         client = get_client()
-        service = client.services.get(id)
-        return render_template("services/inspect.html", service=service)
+        response = client.services.get(id)
+        service = { response }
+        return jsonify( { "service" : service } )
+        # return render_template("services/inspect.html", service=service)
     
     except de.NotFound as nf:
-        return render_template("404.html", error = nf)
+        return jsonify( { "error" : nf } )
+        # return render_template("404.html", error = nf)
     except de.APIError as ae:
-        return render_template("404.html", error = ae)
+        return jsonify( { "error" :  ae } )
+        # return render_template("404.html", error = ae)
     except de.InvalidVersion as iv:
-        return render_template("404.html", error = iv)
+        return jsonify( { "error" :  iv } )
+        # return render_template("404.html", error = iv)
+
+# Swarm Services - Update
+@app.route("/services/update/<id>")
+def swarm_service_update():
+    pass
 
 # Swarm Nodes API
 @app.route("/nodes")
-def list_swarm_nodes():
+def swarm_nodes_list():
     try:
         client = get_client()
         nlist = client.nodes.list()
-        return render_template("nodes/list.html", res_list = nlist)
+        return jsonify( { "nodes list" : nlist } )
+        # return render_template("nodes/list.html", res_list = nlist)
 
     except de.APIError as e:
         flash("Error!")
-        return render_template("404.html", error=e)
+        return jsonify( { "error" : e } )
+        # return render_template("404.html", error=e)
