4d63525346cea6a4dca47fca34007fdb24066507
Abhishek Ganji <abhishekganji27@gmail.com>
2024-10-16 10:56:51 +0530

    modified app

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def get_client():
 def home():
     client = get_client()
     p = client.ping()
-    return jsonify( {"ping": p} )
-    # return render_template('index.html', ping = p)
+    # return jsonify( {"ping": p} )
+    return render_template('index.html', ping = p)
 
 # Swarm Services - List
 @app.route("/services")
@@ -21,12 +21,12 @@ def swarm_services_list():
     try:
         client = get_client()
         slist = client.services.list()
-        return jsonify( { "services list" : slist } )
-        # return render_template("services/list.html", res_list = slist)
+        # return jsonify( { "services list" : slist } )
+        return render_template("services/list.html", res_list = slist)
     except de.APIError as e:
         flash("Error!")
-        return jsonify( { "error" : e } )
-        # return render_template("404.html", error=e)
+        # return jsonify( { "error" : e } )
+        return render_template("404.html", error=e)
 
 
 # Swarm Services - Inspect
@@ -35,19 +35,19 @@ def swarm_service_inspect(id):
     try:
         client = get_client()
         response = client.services.get(id)
-        service = { response }
-        return jsonify( { "service" : service } )
-        # return render_template("services/inspect.html", service=service)
+        # service = { response }
+        # return jsonify( { "service" : service } )
+        return render_template("services/inspect.html", service=response)
     
     except de.NotFound as nf:
-        return jsonify( { "error" : nf } )
-        # return render_template("404.html", error = nf)
+        # return jsonify( { "error" : nf } )
+        return render_template("404.html", error = nf)
     except de.APIError as ae:
-        return jsonify( { "error" :  ae } )
-        # return render_template("404.html", error = ae)
+        # return jsonify( { "error" :  ae } )
+        return render_template("404.html", error = ae)
     except de.InvalidVersion as iv:
-        return jsonify( { "error" :  iv } )
-        # return render_template("404.html", error = iv)
+        # return jsonify( { "error" :  iv } )
+        return render_template("404.html", error = iv)
 
 # Swarm Services - Update
 @app.route("/services/update/<id>")
@@ -60,10 +60,10 @@ def swarm_nodes_list():
     try:
         client = get_client()
         nlist = client.nodes.list()
-        return jsonify( { "nodes list" : nlist } )
-        # return render_template("nodes/list.html", res_list = nlist)
+        # return jsonify( { "nodes list" : nlist } )
+        return render_template("nodes/list.html", res_list = nlist)
 
     except de.APIError as e:
         flash("Error!")
-        return jsonify( { "error" : e } )
-        # return render_template("404.html", error=e)
+        # return jsonify( { "error" : e } )
+        return render_template("404.html", error=e)
